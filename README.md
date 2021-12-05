a20cd0f864957efef4b085821b694f4dc974513d
Vadim Gavrilov <mr.midaw@gmail.com>
2021-12-05 19:04:46 +0300

    add  main page

---
+++ README.md
@@ -0,0 +1,1 @@
+Landing page - Sea Food from Nordway
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/pref.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+        vertical: true
+    });
+});
+
