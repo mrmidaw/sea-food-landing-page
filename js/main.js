e4952a3e66f59304952ff3612b1d927d421a1d48
Vadim Gavrilov <mr.midaw@gmail.com>
2021-12-07 13:53:41 +0300

    add main page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ $(function () {
     });
 });
 
+$('.product__name').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: '.product__content',
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    centerMode: true
+});
+$('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true,
+    arrows: false
+});
+
